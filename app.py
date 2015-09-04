d8ef43850937de18ba2698d8423196d65595ea60
rarenaturew <rarenaturew@users.noreply.github.com>
2015-09-04 10:47:28 -0700

    added user dates

--- app.py
+++ app.py
@@ -18,26 +18,30 @@ def main():
 @app.route('/index',methods=['GET','POST'])
 def index():
   EndDate=datetime.date.today()
-  StartDate = getStartDate(EndDate) # StartDate is a string: %Y-%m-%d
+  StartDate=getStartDate(EndDate) # StartDate is a string: %Y-%m-%d
+  EndDate=EndDate.strftime("%Y-%m-%d") # also in string
   if request.method == 'GET':
     return render_template('index.html',start_date=StartDate)
   else:
     StockName = request.form['stock_name'].upper()
     ShowFeatures=[] # which features to show
-    if request.form.get('close_price',False):
-       ShowFeatures.append('Close')
-    if request.form.get('adj_close_price',False):
-       ShowFeatures.append('Adj. Close')
-    ReURL='https://www.quandl.com/api/v3/datasets/WIKI/'+StockName+'.json?auth_token=XMD-ta2mEyx3bj_AGW3n&start_date='+StartDate+'&end_date='+EndDate.strftime("%Y-%m-%d")
-    r = requests.get(ReURL).json()
+    if request.form.get('close_price',False): ShowFeatures.append('Close')
+    if request.form.get('adj_close_price',False): ShowFeatures.append('Adj. Close')
+    SD=request.form['start_date']
+    if len(SD) == 10 : StartDate=str(SD)
+    ED=request.form['end_date']
+    if len(ED) == 10 : EndDate=str(ED)
+    print StartDate, EndDate
+    print type(StartDate), type(EndDate)
+    URL='https://www.quandl.com/api/v3/datasets/WIKI/'+StockName+'.json?auth_token=XMD-ta2mEyx3bj_AGW3n&start_date='+StartDate+'&end_date='+EndDate
+    r = requests.get(URL).json()
     if 'dataset' in r:
       dataDF = pd.DataFrame(data=r['dataset']['data'],\
                          columns=r['dataset']['column_names'])
       plotDF(dataDF,StockName,ShowFeatures)
       return render_template('bokeh_embed.html') 
     else:
-      message = "Quandl can't find the stock ticker. Please check the spelling and try again."# or: r['quandl_error']['message'] 
-      return render_template('error.html', end_message=message) 
+      return render_template('error.html', end_message=r['quandl_error']['message'] ) 
 
 
 if __name__ == '__main__':
